771c0d7f0b4d3af23dbd7d37f27d0c2908310471
Tanish Singhal <tanishsinghal510@gmail.com>
2024-08-28 17:52:46 +0530

    Restaurant Info UI changed

--- src/components/RestaurantInfo.jsx
+++ src/components/RestaurantInfo.jsx
@@ -1,30 +1,47 @@
 import React from "react";
-import { StarIcon } from "lucide-react";
+import { StarIcon, Clock8, Wallet } from "lucide-react";
 
 const RestaurantInfo = ({ info }) => {
-  const { name, areaName, totalRatingsString, avgRatingString, cuisines } =
-    info;
+  const { 
+    name, 
+    sla = {},
+    areaName, 
+    costForTwoMessage, 
+    totalRatingsString, 
+    avgRatingString, 
+    cuisines = []
+  } = info;
+
+  const { lastMileTravelString = 'N/A', slaString = 'N/A' } = sla;
 
   return (
-    <div className="flex justify-between items-center pb-4 border-b border-dashed">
-      <div>
-        <h2 className="text-xl font-bold my-2">{name}</h2>
-        <p className="text-xs text-gray-500">
-          {cuisines?.map((c, i) => (
-            <span key={i}>
-              {c}
-              {i === cuisines.length - 1 ? "" : ", "}
-            </span>
-          ))}
-        </p>
-        <p className="text-xs text-gray-500">{areaName}</p>
+    <div className="p-6 bg-white shadow-md rounded-lg max-w-screen-lg mx-auto">
+      <div className="flex flex-col lg:flex-row lg:justify-between lg:items-start pb-6 border-b border-gray-300">
+        <div className="mb-6 lg:mb-0 lg:w-2/3">
+          <h2 className="text-3xl lg:text-4xl font-bold text-gray-900 mb-2">{name}</h2>
+          <p className="text-sm lg:text-base text-gray-600 mb-1">
+            {cuisines.join(", ")}
+          </p>
+          <p className="text-sm lg:text-base text-gray-600">
+            {areaName}, {lastMileTravelString}
+          </p>
+        </div>
+        <div className="flex flex-col items-center border rounded-lg border-gray-300 p-4">
+          <p className="flex items-center gap-1 mb-1 text-green-500 text-lg lg:text-xl">
+            <StarIcon className="w-5 h-5 fill-current" /> 
+            <div className="font-semibold">
+            {avgRatingString}
+            </div>
+          </p>
+          <p className="text-sm border-t font-semibold text-gray-500 border-gray-300 pt-1">{totalRatingsString}</p>
+        </div>
       </div>
-      <div className="border rounded-md font-bold  p-2 text-sm">
-        <p className="flex-center gap-1 mb-2 text-green-500 ">
-          <StarIcon className="w-4 h-4" /> {avgRatingString}
+      <div className="mt-6 flex flex-col md:flex-row md:justify-between md:items-center gap-4">
+        <p className="flex items-center gap-2 text-gray-700">
+          <Clock8 className="w-5 h-5 text-gray-500" /> {slaString}
         </p>
-        <p className="pt-2 border-t text-xs font-normal text-gray-500">
-          {totalRatingsString}
+        <p className="flex items-center gap-2 text-gray-700">
+          <Wallet className="w-5 h-5 text-gray-500" /> {costForTwoMessage}
         </p>
       </div>
     </div>
--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import RestaurantInfo from "./RestaurantInfo";
+import MenuItems from "./MenuItems";
 
 const RestaurantMenu = () => {
   const [info, setInfo] = useState([]);
+  const [menu, setMenu] = useState([]);
   const params = useParams();
 
   useEffect(() => {
@@ -12,24 +14,32 @@ const RestaurantMenu = () => {
 
   const fetchMenu = async () => {
     try {
-      const response = await fetch(
-        import.meta.env.VITE_RESTAURANT_MENU_URL + params.id
-      );
+      const response = await fetch(import.meta.env.VITE_RESTAURANT_MENU_URL + params.id);
 
       const json = await response.json();
 
       const info = json.data.cards[2].card.card.info;
-      
-      setInfo(info);
+      const menu = json.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
 
+      setInfo(info);
+      setMenu(menu);
+      
     } catch (error) {
       console.error("Error fetching menu:", error);
     }
   };
 
+  const filterCategories = menu.filter(
+    (c) => c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+  );
+
   return (
     <div className="px-[30rem] my-8">
       <RestaurantInfo info={info} />
+
+      {/* {filterCategories.map((category) => {
+        <MenuItems category={category.card?.card} key={category.card?.card?.title} />;
+      })} */}
     </div>
   );
 };
